be2be48098e014fe00e63a289ec844e973f6d8a5
마현우 <mhw35689402@gmail.com>
2023-08-10 16:17:46 +0900

    feat: 대댓글, 대댓글 좋아요 엔티티 생성

---
+++ src/main/java/kr/kro/schoolzone/schoolzone/domain/replyComments/domain/ReplyComments.java
@@ -0,0 +1,39 @@
+package kr.kro.schoolzone.schoolzone.domain.replyComments.domain;
+
+import jakarta.persistence.*;
+import kr.kro.schoolzone.schoolzone.domain.comments.domain.Comments;
+import kr.kro.schoolzone.schoolzone.domain.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ReplyComments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_comments_id")
+    private Long replyCommentsId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @Column(nullable = false)
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "comments_id")
+    @Column(nullable = false)
+    private Comments commentsId;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Builder
+    public ReplyComments(User userId, Comments commentsId, String contents) {
+        this.userId = userId;
+        this.commentsId = commentsId;
+        this.contents = contents;
+    }
+}
---
+++ src/main/java/kr/kro/schoolzone/schoolzone/domain/replyCommentsLike/domain/ReplyCommentsLike.java
@@ -0,0 +1,35 @@
+package kr.kro.schoolzone.schoolzone.domain.replyCommentsLike.domain;
+
+import jakarta.persistence.*;
+import kr.kro.schoolzone.schoolzone.domain.replyComments.domain.ReplyComments;
+import kr.kro.schoolzone.schoolzone.domain.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ReplyCommentsLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_comments_like_id")
+    private Long replyCommentsLikeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @Column(nullable = false)
+    private User userId;
+
+    @OneToOne
+    @JoinColumn(name = "reply_comments_id")
+    @Column(nullable = false)
+    private ReplyComments replyCommentsId;
+
+    @Builder
+    public ReplyCommentsLike(User userId, ReplyComments replyCommentsId) {
+        this.userId = userId;
+        this.replyCommentsId = replyCommentsId;
+    }
+}
