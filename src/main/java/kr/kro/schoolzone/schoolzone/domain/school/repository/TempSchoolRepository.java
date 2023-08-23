bb7fcaa740e1ac604941061476b7afb2a18dacda
마현우 <mhw35689402@gmail.com>
2023-08-24 01:06:28 +0900

    refactor: 학교 리스트 가져오기는 파이썬으로 변경

--- src/main/java/kr/kro/schoolzone/schoolzone/domain/school/repository/TempSchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.kro.schoolzone.schoolzone.domain.school.repository;
-
-import kr.kro.schoolzone.schoolzone.domain.school.domain.TempSchool;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TempSchoolRepository extends JpaRepository<TempSchool, Long> {
-}
